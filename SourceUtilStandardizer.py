f47a03b2ffab1305d5134d4861c9b6100e955bcc
Alef125 <alifathi.125@gmail.com>
2022-01-31 22:02:05 +0330

    Source Util Standardizer: descriptions + confidence

--- SourceUtilStandardizer.py
+++ SourceUtilStandardizer.py
@@ -1,4 +1,5 @@
-"""
+"""'
+SourceUtilStandardizer
 This script, provides as class to convert some prevalent format for the source utilization growth data, such as .csv,
 into the standard .json format to be used in the further reconstruction steps.
 """
@@ -13,6 +14,10 @@ def get_list_from_comma_separate(comma_separate_str):
 
 class SourceUtilGrowthData:
     def __init__(self, csv_filepath, medium_name):
+        """
+        :param csv_filepath: A string denoting the filepath for the utilization_data.csv
+        :param medium_name: A string denoting the name for this media
+        """
         self.csv_filepath = csv_filepath
         self.medium = medium_name
 
@@ -20,11 +25,14 @@ class SourceUtilGrowthData:
         csv_file = pd.read_csv(self.csv_filepath)
         util_dicts = []
         for index, row in csv_file.iterrows():
+            confidence_score = None
+            if 'Confidence Score' in row.keys():
+                confidence_score = row['Confidence Score']
             util_dicts.append(
                 {'sources_id': get_list_from_comma_separate(row['IDs']),
                  'medium': self.medium,
                  'growth': row['Growth'],
-                 'confidence_sc': row['Confidence Score']}
+                 'confidence_sc': confidence_score}
             )
         with open(filepath_to_save, 'w', encoding='utf-8') as f:
             json.dump(util_dicts, f, ensure_ascii=False, indent=4)
